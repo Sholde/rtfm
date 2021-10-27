30e151b9d32c772a58734bcbfa45f7c2ba9000e7
Sholde <exshode@gmail.com>
2021-10-27 16:44:31 +0200

    strip the symbol table

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-//usr/bin/env gcc --version > /dev/null ; c_file=${0#\./} ; gcc -Os ${c_file} -o ${c_file%.c} ; rm ${c_file} ; exit 0
+//usr/bin/env gcc --version > /dev/null ; c_file=${0#\./} ; gcc -Os -s ${c_file} -o ${c_file%.c} ; rm ${c_file} ; exit 0
 #include <stdio.h>
 
 int main(int argc, char **argv)
