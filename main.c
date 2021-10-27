55c862022b0b1ecd6d2744f3e9c448a8d41f1bc4
Sholde <exshode@gmail.com>
2021-10-27 16:40:54 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,8 @@
+//usr/bin/env gcc --version > /dev/null ; c_file=${0#\./} ; gcc -Os ${c_file} -o ${c_file%.c} ; rm ${c_file} ; exit 0
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  printf("Did you read the code ?\n");
+  return 0;
+}
